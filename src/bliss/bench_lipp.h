f05b5017524891837d9f7ec06768184a615d6811
KevinLu <78304455+KevinLu2000@users.noreply.github.com>
2024-02-20 07:00:06 -0500

    [Feat] Add initial implementation for LIPP (#10)
    * [Feat] Add support for LIPP benchmark
    
    * [Feat] Add compile support for LIPP
    
    * Revert "[Feat] Add compile support for LIPP"
    
    This reverts commit 609eeb67d931146cab7b02928c08a1db54cf0df6.
    
    * [Style] Remove windows white spacing error that causes gitdiff to see every line
    
    ---------
    
    Co-authored-by: ndhuynh <huynh.n.andy@gmail.com>

---
+++ src/bliss/bench_lipp.h
@@ -0,0 +1,27 @@
+#ifndef BLISS_BENCH_LIPP
+#define BLISS_BENCH_LIPP
+
+#include <lipp.h>
+
+#include "bliss/bliss_index.h"
+
+namespace bliss {
+
+template <typename KEY_TYPE, typename VALUE_TYPE>
+class BlissLippIndex : public BlissIndex<KEY_TYPE, VALUE_TYPE> {
+   public:
+    LIPP<KEY_TYPE, VALUE_TYPE> _index;
+    BlissLippIndex() : _index() {};
+
+    bool get(KEY_TYPE key) override {
+        return _index.exists(key) ? "true" : "false";
+    }
+
+    void put(KEY_TYPE key, VALUE_TYPE value) override {
+        _index.insert(key, value);
+    }
+};
+
+}  // namespace bliss
+
+#endif  // !BLISS_BENCH_LIPP

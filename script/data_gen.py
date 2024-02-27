3723daf2326c03e4201399e38e91f54c695dd9b0
ndhuynh <huynh.n.andy@gmail.com>
2024-02-27 14:32:22 -0500

    [Feat] Add cmake git submodule init

--- script/data_gen.py
+++ script/data_gen.py
@@ -3,10 +3,9 @@ import os
 import argparse
 from infra.pybods import PyBods
 
-# K_CHOICES = [1, 3, 5, 10, 25, 50]
 K_CHOICES = [1, 3, 5, 10, 25, 50]
-L_CHOICES = [100]
-# SPECIAL_KL = ((0, 0), (100, 100))
+L_CHOICES = [1, 3, 5, 10, 25, 50, 100]
+SPECIAL_KL = [(0, 0), (100, 100)]
 
 NUM_KEYS = 500_000_000
 DOMAIN_SIZE = 500_000_000
@@ -15,7 +14,8 @@ ALPHA = 1
 BETA = 1
 PAYLOAD = 0
 SEED = 0
-BINARY_FILE_FORMAT = True
+BINARY_FILE_FORMAT = False
+
 
 def create_file_str(
     num_entry: int,
@@ -23,14 +23,9 @@ def create_file_str(
     l: float,
     binary: bool = False,
 ) -> str:
-    file_str = (
-        f"data_N{num_entry}"
-        f"_k{k}"
-        f"_l{l}"
-        ".bin" if binary else ".txt"
-    )
+    file_str = [f"data_N{num_entry}", f"_k{k}", f"_l{l}", ".bin" if binary else ".txt"]
 
-    return file_str
+    return "".join(file_str)
 
 
 def main(args):
@@ -38,13 +33,11 @@ def main(args):
 
     kl_vals = ((k_pt, l_pt) for k_pt in K_CHOICES for l_pt in L_CHOICES)
 
-    for k_pt, l_pt in kl_vals:
+    for k_pt, l_pt in list(kl_vals) + SPECIAL_KL:
         file_name = create_file_str(
-            num_entry=NUM_KEYS,
-            k=k_pt,
-            l=l_pt,
-            binary=BINARY_FILE_FORMAT
+            num_entry=NUM_KEYS, k=k_pt, l=l_pt, binary=BINARY_FILE_FORMAT
         )
+        print(f"Creating {file_name}")
         results = bods.gen_data(
             output_file=os.path.join(args.output_folder, file_name),
             num_entry=NUM_KEYS,
@@ -59,28 +52,6 @@ def main(args):
             binary_file_format=BINARY_FILE_FORMAT,
         )
         print(results)
-
-    # for k_pt, l_pt in SPECIAL_KL:
-    #     file_name = create_file_str(
-    #         num_entry=NUM_KEYS,
-    #         k=k_pt,
-    #         l=l_pt,
-    #         binary=BINARY_FILE_FORMAT
-    #     )
-    #     results = bods.gen_data(
-    #         output_file=os.path.join(args.output_folder, file_name),
-    #         num_entry=NUM_KEYS,
-    #         k=k_pt,
-    #         l=l_pt,
-    #         alpha=ALPHA,
-    #         beta=BETA,
-    #         domain=DOMAIN_SIZE,
-    #         window_size=WINDOW,
-    #         payload=PAYLOAD,
-    #         seed=SEED,
-    #         binary_file_format=BINARY_FILE_FORMAT,
-    #     )
-    #     print(results)
 
 
 if __name__ == "__main__":

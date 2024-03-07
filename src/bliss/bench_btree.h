5e16e3563200f037692af59f69463a905f462262
KevinLu <78304455+KevinLu2000@users.noreply.github.com>
2024-03-07 13:51:17 -0500

    [Chore] Merge conflicts

--- src/bliss/bench_btree.h
+++ src/bliss/bench_btree.h
@@ -1,9 +1,8 @@
 #ifndef BLISS_BENCH_BTREE
 #define BLISS_BENCH_BTREE
 
-#include "tlx/container/btree_map.hpp"
+#include "tlx/container/btree.hpp"
 #include <vector>
-#include <algorithm>
 #include "bliss/bliss_index.h"
 
 namespace bliss {
@@ -11,23 +10,20 @@ namespace bliss {
 template <typename KEY_TYPE, typename VALUE_TYPE>
 class BlissBTreeIndex : public BlissIndex<KEY_TYPE, VALUE_TYPE> {
    public:
-    tlx::btree_map<KEY_TYPE, VALUE_TYPE> _index;
+    tlx::btree_default_traits<KEY_TYPE, VALUE_TYPE> _index;
     BlissBTreeIndex() = default;
 
-    // bulk load
     void preload(std::vector<std::pair<KEY_TYPE, VALUE_TYPE>> values) override {
-        // Value has been pre-sorted
-        // Insert sorted values into the B+ tree
+        // expects the pairs to be pre-sorted before performing bulk load
         _index.bulk_load(values.begin(), values.end());
     }
 
     bool get(KEY_TYPE key) override {
-        auto it = _index.find(key);
-        return it != _index.end();
+        return _index.exists(key);
     }
 
     void put(KEY_TYPE key, VALUE_TYPE value) override {
-       _index.insert(std::make_pair(key, value));
+        _index.insert(key, value);
     }
 };
 

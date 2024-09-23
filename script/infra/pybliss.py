2d37791d644f72ec386dcb7765f37d27a1320dcf
Lucas <junsuny789@gmail.com>
2024-09-23 01:17:49 -0400

    adding ART

--- script/infra/pybliss.py
+++ script/infra/pybliss.py
@@ -68,6 +68,7 @@ class PyBliss:
             f"--file_type {'binary' if args.file_type else 'txt'}",
             "--use_preload" if args.use_preload else "",
         ]
+        print("this is cmd: " , " ".join(cmd))
         process = subprocess.Popen(
             " ".join(cmd),
             stdout=subprocess.PIPE,

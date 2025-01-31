381ca5b0a11c7835c992b7d00d0a99f5a2816c91
Yanpeng Wei <ywe1@pilaros.bu.edu>
2025-01-31 07:30:05 -0500

    initial commit

--- src/bliss_bench.cpp
+++ src/bliss_bench.cpp
@@ -9,6 +9,7 @@
 #include "bliss/bench_alex.h"
 #include "bliss/bench_btree.h"
 #include "bliss/bench_lipp.h"
+#include "bliss/bench_columnsketches.h"
 #include "bliss/bliss_index.h"
 #include "bliss/util/args.h"
 #include "bliss/util/config.h"
@@ -168,6 +169,8 @@ int main(int argc, char *argv[]) {
         index.reset(new bliss::BlissLippIndex<key_type, value_type>());
     } else if (config.index == "btree") {
         index.reset(new bliss::BlissBTreeIndex<key_type, value_type>());
+    } else if (config.index == "columnskteches") {
+        index.reset(new bliss::BlissColumnSketchesIndex<key_type, value_type>());
     } else {
         spdlog::error(config.index + " not implemented yet", 1);
     }
